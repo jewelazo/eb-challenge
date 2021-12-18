c4b6d1c054c93ed6238fae85c16808430fe95ac3
jewelazo <gpalominoe@uni.pe>
2021-12-18 22:55:24 +0000

    fix css of show view

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,8 +1,9 @@
 class PropertiesController < ApplicationController
     include HTTParty
     BASE_URL="https://api.stagingeb.com/v1"
+    PARTIAL_URL_INITIAL_PAGE="/properties?page=1&limit=15&search%5Bstatuses%5D%5B%5D=published"
     def index
-        response=HTTParty.get("#{BASE_URL}/properties?page=1&limit=15&search%5Bstatuses%5D%5B%5D=published",
+        response=HTTParty.get("#{BASE_URL}#{PARTIAL_URL_INITIAL_PAGE}",
                     headers: { "X-Authorization" => "l7u502p8v46ba3ppgvj5y2aad50lb9"})
         @properties=JSON.parse(response.body,symbolize_names: true)[:content]
     end
@@ -31,7 +32,7 @@ class PropertiesController < ApplicationController
             redirect_to root_path
         else
             flash.alert = "You must specify a contact source"
-            redirect_to "/properties/#{params[:property_id]}/new_contact"
+            redirect_to "/properties/#{params[:property_id]}"
         end
     end
 
